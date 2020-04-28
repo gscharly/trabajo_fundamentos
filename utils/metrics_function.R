ec15f723ba42db79940343fb2e13c90cfdff5eb1
gscharly <c.gomezs.2019@alumnos.urjc.es>
2020-04-28 20:32:04 +0200

    reorganization

--- utils/metrics_function.R
+++ utils/metrics_function.R
@@ -38,3 +38,47 @@ metrics_function <- function(prediccion, test, label, bool){
   return(list(metrics, conf_mat))
   
 }
+
+# Función para el cáculo de las métricas
+#Input necedario son las predicciones del modelo, el conjunto de datos y el nombre de la columna a predecir
+metrics_function_num <- function(preds, test, label){
+  accuracy = sum(preds == test[,label]) / nrow(test)
+  recall = sum(preds == test[,label] & test[,label] == 1) / sum(test[,label] == 1)
+  precision = sum(preds == test[,label] & test[,label] == 1) / sum(preds == 1)
+  f1_score = 2*recall*precision/(recall+precision)
+  specificity = sum(preds == test[,label] & test[,label] == 0) / sum(test[,label] == 0)
+  metrics <- c(accuracy = accuracy, recall = recall, specificity = specificity, precision = precision, f1=f1_score)
+  cm <- caret::confusionMatrix(preds, test[,label], positive = '1')
+  return(list(metrics, cm))
+}
+
+
+# Funciones para dibujar matriz de confusión y curva ROC
+#Input necesario es una confusionMatrix de caret
+ggplotConfusionMatrix <- function(m){
+  mytitle <- paste("Accuracy", percent_format()(m$overall[1]))
+  p <-
+    ggplot(data = as.data.frame(m$table) ,
+           aes(x = Reference, y = Prediction)) +
+    geom_tile(aes(fill = log(Freq)), colour = "white") +
+    scale_fill_gradient(low = "white", high = "steelblue") +
+    geom_text(aes(x = Reference, y = Prediction, label = Freq)) +
+    theme(legend.position = "none") +
+    ggtitle(mytitle)
+  return(p)
+}
+
+# Input necesario es un pROC object
+ggplotROCCurve <- function(roc_object, interval = 0.2, breaks = seq(0, 1, interval)){
+  require(pROC)
+  if(class(roc) != "roc")
+    simpleError("Please provide roc object from pROC package")
+  ggroc(roc_object) + 
+    scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) + 
+    scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
+    geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.01) + 
+    theme_bw() + 
+    theme(axis.ticks = element_line(color = "grey80")) +
+    coord_equal() + 
+    annotate("text",x=-Inf,y=-Inf, label = paste("AUC =",sprintf("%.3f",roc_object$auc)), hjust= 1.2, vjust = -7)
+}
