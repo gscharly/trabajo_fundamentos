d230c7de37d0b1cd902dcd5421b9a4d9d981c3c2
gscharly <c.gomezs.2019@alumnos.urjc.es>
2020-05-02 11:34:19 +0200

    Added new plot function

--- utils/metrics_function.R
+++ utils/metrics_function.R
@@ -53,35 +53,6 @@ metrics_function_num <- function(preds, test, label){
 }
 
 
-# Funciones para dibujar matriz de confusión y curva ROC
-#Input necesario es una confusionMatrix de caret
-ggplotConfusionMatrix <- function(m){
-  mytitle <- paste("Accuracy", percent_format()(m$overall[1]))
-  p <-
-    ggplot(data = as.data.frame(m$table) ,
-           aes(x = Reference, y = Prediction)) +
-    geom_tile(aes(fill = log(Freq)), colour = "white") +
-    scale_fill_gradient(low = "white", high = "steelblue") +
-    geom_text(aes(x = Reference, y = Prediction, label = Freq)) +
-    theme(legend.position = "none") +
-    ggtitle(mytitle)
-  return(p)
-}
-
-# Input necesario es un pROC object
-ggplotROCCurve <- function(roc_object, interval = 0.2, breaks = seq(0, 1, interval)){
-  require(pROC)
-  if(class(roc) != "roc")
-    simpleError("Please provide roc object from pROC package")
-  ggroc(roc_object) + 
-    scale_x_reverse(name = "Specificity",limits = c(1,0), breaks = breaks, expand = c(0.001,0.001)) + 
-    scale_y_continuous(name = "Sensitivity", limits = c(0,1), breaks = breaks, expand = c(0.001, 0.001)) +
-    geom_segment(aes(x = 0, y = 1, xend = 1,yend = 0), alpha = 0.01) + 
-    theme_bw() + 
-    theme(axis.ticks = element_line(color = "grey80")) +
-    coord_equal() + 
-    annotate("text",x=-Inf,y=-Inf, label = paste("AUC =",sprintf("%.3f",roc_object$auc)), hjust= 1.2, vjust = -7)
-}
 
 perform_knn <- function(train, test, label, k, cols) {
   predictions <- knn(train[,cols], test[,cols], k=k, cl=train[,label])
@@ -90,39 +61,6 @@ perform_knn <- function(train, test, label, k, cols) {
   
 }
 
-plot_acc_f1_k <- function(train, label, cols){
-  long = 15
-  accuracy = rep(0,long)
-  f1score = rep(0,long)
-  recall = rep(0,long)
-  precision = rep(0,long)
-  for (i in 1:long)
-  {
-    prediccion_knn_cv =knn.cv(train[,cols], 
-                              k=i, cl=train[,label])
-    accuracy[i] = sum(prediccion_knn_cv == train[,label]) /nrow(train)
-    recall[i] = sum(prediccion_knn_cv == train[,label] & train[,label] == TRUE) / sum(train[,label] == TRUE)
-    precision[i] = sum(prediccion_knn_cv == train[,label] & prediccion_knn_cv == TRUE) / sum(prediccion_knn_cv == TRUE)
-    f1score[i] = 2*precision[i]*recall[i]/(precision[i]+recall[i])
-  }
-  resultados_knn = as.data.frame(cbind(accuracy,f1score,precision,recall))
-  resultados_knn = resultados_knn %>% mutate(index=as.factor(seq(1:long)))
-  
-  max(resultados_knn$f1score)
-  which.max(resultados_knn$f1score)
-  
-  
-  p1 <- ggplot(data=resultados_knn,aes(x=index,y=accuracy)) + 
-    geom_col(colour="cyan4",fill="cyan3")+
-    ggtitle("Accuracy")
-  
-  
-  p2 <- ggplot(data=resultados_knn,aes(x=index,y=f1score)) + 
-    geom_col(colour="orange4",fill="orange3") +
-    ggtitle("F1_score values")
-  
-  plot_grid(p1, p2, rel_heights = c(1/2, 1/2))
-}
 
 dt_metrics <- function(df, features, label){
   df_f <- df[c(features, label)]
